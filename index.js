cc3f99aefa6e2af844ccc5b2d80f202f276dbca9
Mydul Islam <si240147@gmail.com>
2025-02-11 00:32:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// const colors = ["red", "blue", "green", "yellow", "orange"];
+// let color = colors.reverse();
+// console.log(color);
+
+// // task 2
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// const evenNumbers = [];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 === 0) {
+//     evenNumbers.push(numbers[i]);
+//   }
+// }
+// console.log(evenNumbers);
+// again
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// const evenNumber = [];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 === 0) {
+//     evenNumber.push(numbers[i]);
+//   }
+// }
+// console.log(evenNumber);
+
+// task-3
+// var numbers = ["Tom", "Tim", "Tin", "Tik"];
+// var result = [];
+// for (let name of numbers) {
+//   result = result + name;
+// }
+// console.log(result);
+
+// task-3
+// var numbers = ["Tom", "Tim", "Tin", "Tik"];
+// var results = numbers.join("");
+// console.log(results);
+
+// task-4
+// const statement = "I am a hard working person";
+// const reverseing = statement.split(" ").reverse().join(" ");
+// console.log(reverseing);
+
+const statement = "I am a hard working person";
+let total = statement.split(" ").reverse().join(" ");
+console.log(total);
---
+++ looping.js
@@ -0,0 +1,46 @@
+// const friends = ["mydul", "mim", "miran", "hridoy"];
+// for (const friend of friends) {
+//   //   console.log(friend);
+// }
+// for (let i = 0; i <= friends.length; i++) {
+//   console.log(i);
+//   console.log(friends[i]);
+// }
+
+// let totalls = [22, 44, 11, 2, 3, 4, 5, 66, 88, 76, 4, 654];
+// for (let i = 0; i < totalls.length; i++) {
+//   console.log(totalls[i]);
+// }
+
+// const friends = ["mydul", "mim", "miran", "hridoy", "kona"];
+// let i = 0;
+// while (i < friends.length) {
+//   console.log(friends[i]);
+//   i++;
+// }
+
+// reverse
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// numbers.reverse();
+// console.log(numbers);
+
+// friends.reverse();
+// console.log(friends);
+
+// short javascript mane serial vabe dibe
+
+const persons = ["rakib", "mydul", "mim", "miran"];
+persons.sort();
+console.log(persons);
+
+const numbers = [4, 2, 545, 7, 22, 1];
+const numbers_ac = numbers.sort(function (a, b) {
+  return a - b;
+});
+console.log(numbers_ac);
+
+const number_dc = numbers.sort(function (a, b) {
+  return b - a;
+});
+console.log(number_dc);
